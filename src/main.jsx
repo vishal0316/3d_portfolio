0136813bdfc5a7d21d7a1fc7d2f1f9493850a477
vishal rajput <vr987195@gmail.com>
2023-11-11 17:08:03 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
-import ReactDOM from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
