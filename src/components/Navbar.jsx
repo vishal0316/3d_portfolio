4023e60aa7fb2ffbc9283e0b633b82d813288f76
vishal rajput <vr987195@gmail.com>
2023-11-12 15:30:39 +0530

    nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">VR</p>
+      </NavLink>
+      <nav>
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
