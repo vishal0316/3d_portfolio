991a7b7757c8f9ae13d086da12c4f2d065b3f799
vishal rajput <vr987195@gmail.com>
2023-11-14 16:26:25 +0530

    bg

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -5,7 +5,7 @@ import { arrow } from "../assets/icons";
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
-      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5 ">
         Hi, I'm
         <span className="font-semibold mx-2 text-white">Vishal</span>
         👋
@@ -47,7 +47,7 @@ const HomeInfo = ({ currentStage }) => {
 
   if (currentStage === 4) {
     return (
-      <div className="info-box">
+      <div className="info-box  ">
         <p className="font-medium sm:text-xl text-center">
           Need a project done or looking for a dev? <br /> I'm just a few
           keystrokes away
