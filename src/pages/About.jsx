17e16ab6541b40c419240bd828ee22e53e1d1fb9
vishal rajput <vr987195@gmail.com>
2023-11-15 15:22:41 +0530

    constants

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+
 const About = () => {
-  return <section className="max-container">
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        {" "}
+        Hello , I'm <span className="blue-gradient_text ">Vishal</span>
+      </h1>
 
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          A passionate Fullstack Developer with a background in Computer
+          Applicatons. Currently, learning and building applications.
+        </p>
+      </div>
 
-    
-  </section>;
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text"> My Skills </h3>
+        <div className="mt-16 flex flex-wrap gap-12"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
