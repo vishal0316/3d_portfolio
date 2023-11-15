bc68af242f95ea43f27d53c3f1e4546c52cbebe4
vishal rajput <vr987195@gmail.com>
2023-11-15 15:02:18 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <section className="max-container">
 
-export default About
+
+    
+  </section>;
+};
+
+export default About;
