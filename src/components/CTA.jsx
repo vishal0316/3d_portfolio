2ff4fca6bf502bcce6ad195762fdf8462efbc6cb
vishal rajput <vr987195@gmail.com>
2023-12-24 17:28:06 +0530

    contact

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FaTwitter, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const CTA = () => {
   return (
@@ -7,6 +8,25 @@ const CTA = () => {
         Have a project in mind? <br className="sm:block hidden" />
         Let’s build something together!
       </p>
+      <div className="cta-buttons">
+        <a
+          href="https://twitter.com/vishalll141"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTwitter className="cta-icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vishal-rajput-1603v/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="cta-icon" />
+        </a>
+        <a href="mailto:vishal.rajput.work@gmail.com">
+          <FaEnvelope className="cta-icon" />
+        </a>
+      </div>
       <Link to="/contact" className="btn">
         Contact
       </Link>
